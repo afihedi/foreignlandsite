6d9ea2263a40d70de1f3f2a5e692d737169a70b4
afihedi <78396473+afihedi@users.noreply.github.com>
2023-01-11 16:21:56 -0500

    Add files via upload

---
+++ subscriber.php
@@ -0,0 +1,16 @@
+<?php
+
+
+if($_POST["email"]) {
+
+
+mail("faihedi23@gmail.com", "Foreign Land Drop 1: Something Different",
+
+
+$_POST["It's coming. And you better be ready. FL 2023"]. "From: foreignlandssales@gmail.com");
+
+
+}
+
+
+?>
